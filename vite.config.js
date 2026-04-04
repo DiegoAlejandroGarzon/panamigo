915438e257ef66a36c462bb70fae2e7751b6d3c8
DiegoAlejandroGarzon <tingonay10@hotmail.com>
2026-04-03 21:28:15 -0500

    fix ticket mobile

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     server: {
         host: '0.0.0.0',
         hmr: {
-            host: '127.0.0.1',
+            host: '192.168.101.11',
         },
     },
     build: {
